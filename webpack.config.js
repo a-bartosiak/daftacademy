438fd13603920dfddcedaf9dd6fecbcde6671f86
Aleksandra Bogdanska <alexandra.bogdanska@gmail.com>
2019-03-25 20:44:25 +0100

    added project

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist') },
+    mode: 'development', // 'production'
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin()]
+};
